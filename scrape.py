76c56f445696bbaa0da533fdebb099ce6d24df87
Vail Dorchester <vail.dorchester@colorado.edu>
2019-09-19 13:53:48 -0600

    Add files via upload
    s

---
+++ scrape.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import pandas as pd
+
+# this function will get the links to every monster
+# returns dataframe with name and url and key
+def get_df():
+    # set the url
+    url = "https://jsigvard.com/dnd/Monsters.html"
+    request = requests.get(url) # send a request
+    content = request.content # get the content
+    soup = BeautifulSoup(content, "html.parser") # parse it
+
+    # gets the links associated with each entry
+    monsters = soup.select("a:link")
+    links = {}
+    for link in monsters:
+        sub_link = re.findall(r'"(.*?)"', str(link))[0]
+        name = re.findall(r'>(.*?)<', str(link))[0]
+        sub_link = sub_link.replace(" ", "%20")
+        links[name] = sub_link
+
+    # now put it in a dataframe
+    names = links.keys()
+    urls = links.values()
+    df = pd.DataFrame(list(zip(names, urls)), columns = ['name','url'])
+    return df
+
+# creates the full links
+def create_links(base, extensions):
+    links = []
+    for li in extensions:
+        links.append(base+li)
+    return links
+
+def main():
+    base_link = 'https://jsigvard.com/dnd/' # add the links to this to get data
+    monsters = get_df() # get the monster dataframe
+    links = create_links(base_link, list(monsters.url)) # get the full urls
+    monsters['full_url'] = links # add to dataframe
+    print(monsters.head())
+
+if __name__=="__main__":main()
