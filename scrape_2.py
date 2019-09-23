8b2fd63aa4d879cbccad0362b04a7deb03c353ed
VDorchester14 <vail.dorchester@colorado.edu>
2019-09-23 11:22:07 -0600

    minor changes

--- scrape_2.py
+++ scrape_2.py
@@ -131,7 +131,7 @@ def main():
     #url = monsters.iloc[0].full_url
     #monsters = get_monster_data(monsters)
     #monsters.to_pickle("monsters.pkl")
-    monstesr = pd.read_pickle("monsters.pkl")
+    monsters = pd.read_pickle("monsters.pkl")
     print(monsters.head(3))
 
 
